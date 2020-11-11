b498dd71e0b30a49b027787303553bee574f645f
Thomas Burkhalter <new.mysteryman@gmail.com>
2020-11-11 11:56:34 +0100

    Add organization specific permissions

--- app/permissions/decidim_zuerich/participatory_processes/permissions.rb
+++ app/permissions/decidim_zuerich/participatory_processes/permissions.rb
@@ -1,14 +1,25 @@
+# frozen_string_literal: true
+
+# Customize participatory permissions
 module DecidimZuerich
   module ParticipatoryProcesses
     module Permissions
-      # Process admins can eprform everything *inside* that process. They cannot
-      # create a process or perform actions on process groups or other
-      # processes.
       def process_admin_action?
         result = super
         return result if result
 
-        allow! if permission_action.subject == :space_private_user
+        allow_space_private_user?
+      end
+
+      private
+
+      def allow_space_private_user?
+        return unless permission_action.subject == :space_private_user
+
+        allowed_orgs = [1]
+        current_org = context.fetch(:current_organization, nil)&.id
+
+        allow! if allowed_orgs.include?(current_org)
       end
     end
   end
