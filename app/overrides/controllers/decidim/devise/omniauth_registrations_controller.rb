c8e5565e6da1ff37f0a22b4b12dc320cea94f239
Carlo Beltrame <carlo@beltra.me>
2021-04-07 13:51:53 +0200

    Propose name and nickname when signing up with Mein Konto

---
+++ app/overrides/controllers/decidim/devise/omniauth_registrations_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+Decidim::Devise::OmniauthRegistrationsController.class_eval do
+
+  private
+  def user_params_from_oauth_hash
+    return nil if oauth_data.empty?
+
+    name = [oauth_data[:info][:first_name], oauth_data[:info][:last_name]].join(' ')
+
+    {
+        provider: oauth_data[:provider],
+        uid: oauth_data[:uid],
+        # Modification for Mein Konto
+        name: name,
+        # Modification for Mein Konto
+        nickname: name.parameterize(separator: '_'),
+        oauth_signature: Decidim::OmniauthRegistrationForm.create_signature(oauth_data[:provider], oauth_data[:uid]),
+        avatar_url: oauth_data[:info][:image],
+        raw_data: oauth_hash
+    }
+  end
+end
