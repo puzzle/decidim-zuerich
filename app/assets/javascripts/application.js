136277de7a68f918ae305fc72163047f0cf1acf4
Carlo Beltrame <carlo@beltra.me>
2021-07-15 17:28:46 +0200

    Implement semi-sticky search bar in header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,37 @@
 //= require cable.js
 //= require decidim
 //= require custom.js
+
+
+/**
+ * Make the search bar (topbar) in the page header scroll out of sight AFTER
+ * the logobar below it. This is acheived by the following setup:
+ *
+ * - The topbar comes AFTER the logobar in the HTML markup
+ * - Initially, the topbar is visually moved above the logobar using position:fixed and
+ *   padding-top on the containing header div. This way, the topbar sticks to the top of
+ *   the screen.
+ * - As soon as the user scrolls so far that the logobar is completely out of sight,
+ *   we remove the fixed position and the top padding, so the topbar is now visually
+ *   below the (out of sight) logobar.
+ * - When the user scrolls back up, we restore the positioning from before.
+ *
+ */
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+
+  // The bar containing the search box
+  const topbar = document.getElementById('topbar')
+  // The bar containing the application logo
+  const logobar = document.getElementById('logobar')
+  // The header div that wraps both the above bars
+  const header = document.getElementById('header')
+
+  if (currentScroll > logobar.offsetHeight) {
+    topbar.style.position = 'static'
+    header.style.paddingTop = 0
+  } else {
+    topbar.style.position = 'fixed'
+    header.style.paddingTop = '3.75rem'
+  }
+})
