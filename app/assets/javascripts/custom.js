74b6c837c712693cab13b87f62ec24350912ea3a
Carlo Beltrame <carlo@beltra.me>
2021-11-24 11:34:04 +0100

    Redirect to external participatory processes
    To set up a redirect, just add any link to the announcement of a
    participatory process.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,22 @@
 window.addEventListener("load", (event) => {
   const process_nav = document.querySelector('#process-nav-content');
+  if (process_nav == null) {
+    return;
+  }
+
   const active_child = process_nav.querySelector('li + li.is-active');
 
   if (active_child != null) {
     process_nav.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+  }
+});
+
+/**
+ * Redirect to external participatory processes
+ */
+window.addEventListener("load", (event) => {
+  const redirectLink = document.querySelector('.process-header + .wrapper .callout.announcement a');
+  if (redirectLink != null) {
+    window.location.replace(redirectLink.getAttribute('href'));
   }
 });
