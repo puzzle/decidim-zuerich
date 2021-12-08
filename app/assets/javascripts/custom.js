3bb11011fed43f07783e3865bfc74332bc814962
Carlo Beltrame <carlo@beltra.me>
2021-12-08 17:27:54 +0100

    Use jQuery instead of load event because it is more robust and we have jQuery anyways

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", (event) => {
+$(() => {
   const process_nav = document.querySelector('#process-nav-content');
   if (process_nav == null) {
     return;
@@ -14,26 +14,16 @@ window.addEventListener("load", (event) => {
 /**
  * Redirect to external participatory processes
  */
-console.log('Installing event listener on load event')
-window.addEventListener("load", (event) => {
-  console.log('load event was triggered');
+$(() => {
   const announcement = document.querySelector('.process-header + .wrapper .callout.announcement');
-  console.log('found announcement:', announcement);
   if (announcement == null) return;
 
   // Convention: Only redirect if the announcement text starts and ends with three dashes.
   const text = announcement.textContent.trim();
-  console.log('announcement has text:', text);
   if (text.match(/^---.*---$/) === null) return;
-  console.log('text matches the --- convention');
 
   const redirectLink = announcement.querySelector('a');
-  console.log('found redirect link in announcement:', redirectLink);
   if (redirectLink == null || !redirectLink.getAttribute('href')) return;
-  console.log('redirectLink has href', redirectLink.getAttribute('href'));
 
-  console.log('we are currently at', window.location, 'but now we will redirect...');
   window.location.replace(redirectLink.getAttribute('href'));
-  console.log('redirect command has finished');
 });
-console.log('finished installing event listener');
