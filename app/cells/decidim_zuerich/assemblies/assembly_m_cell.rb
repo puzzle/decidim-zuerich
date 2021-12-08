0f3fd558b3d05a471f8b766be7e2f11b8fa6eced
Thomas Burkhalter <new.mysteryman@gmail.com>
2021-12-08 15:41:43 +0100

    Add simplified Assembly view analog to dialogluzern.ch
    Closes #192

--- app/cells/decidim_zuerich/assemblies/assembly_m_cell.rb
+++ app/cells/decidim_zuerich/assemblies/assembly_m_cell.rb
@@ -2,6 +2,7 @@
 
 module DecidimZuerich
   module Assemblies
+    # Modifications to make the /assemblies list more compact
     module AssemblyMCell
       include ApplicationHelper
 
@@ -9,11 +10,32 @@ module DecidimZuerich
         true
       end
 
+      def followers_count
+        if model.respond_to?(:followers_count)
+          model.followers_count
+        else
+          model.followers.count
+        end
+      end
+
+      def has_badge?
+        false
+      end
+
+      def description
+        nil
+      end
+
       private
 
       def resource_image_path
         model.hero_image.url.presence || 'organization-default-image.png'
       end
+
+      def statuses
+        []
+      end
+
     end
   end
 end
