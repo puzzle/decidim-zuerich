d51d4db18acec6ecff7977b27589040cf7513025
Carlo Beltrame <carlo@beltra.me>
2022-02-25 15:58:10 +0100

    Silence spammy warnings

--- lib/overrides/lib/decidim/content_renderers/hashtag_renderer_override.rb
+++ lib/overrides/lib/decidim/content_renderers/hashtag_renderer_override.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-Decidim::ContentRenderers::HashtagRenderer.const_set(
-  :GLOBAL_ID_REGEX,
-  %r{gid://(?!.*gid://)[\w-]*/Decidim::Hashtag/(\d+)/?(_?)([[:alnum:]](?:[[:alnum:]]|_)*)?\b}.freeze
-)
+# TODO remove this after upgrading to decidim 0.26
+
+Kernel::silence_warnings do
+  Decidim::ContentRenderers::HashtagRenderer.const_set(
+    :GLOBAL_ID_REGEX,
+    %r{gid://(?!.*gid://)[\w-]*/Decidim::Hashtag/(\d+)/?(_?)([[:alnum:]](?:[[:alnum:]]|_)*)?\b}.freeze
+  )
+end
