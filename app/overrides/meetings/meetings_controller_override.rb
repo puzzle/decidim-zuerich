265ffa640cb068f7d78a9a65951c127335825f6b
Thomas Burkhalter <burkhalter@puzzle.ch>
2022-06-16 15:44:56 +0200

    Readd base meetings_controller sorting

---
+++ app/overrides/meetings/meetings_controller_override.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Decidim::Meetings::MeetingsController.class_eval do
+  def meetings
+    require 'pry'; binding.pry
+    @meetings ||= paginate(search.results.order(:start_time))
+  end
+end
