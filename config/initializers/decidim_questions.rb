402095e1cd9ed359ead043cf66509676f6ec2476
Thomas Burkhalter <burkhalter@puzzle.ch>
2022-11-18 08:43:11 +0100

    Update captcha question

--- config/initializers/decidim_questions.rb
+++ config/initializers/decidim_questions.rb
@@ -2,13 +2,13 @@ Decidim::QuestionCaptcha.configure do |config|
   config.questions = {
     de: [
       {
-        'question' => 'Nennen Sie eine der beiden Farben des Wappens der Stadt Zürich.',
+        'question' => 'Nennen Sie eine Farbe des Zürcher Wappens.',
         'answers' => 'weiss,blau,Weiss,Blau'
       }
     ],
     en: [
       {
-        'question' => 'Name one of the two colors of the city of Zurich coat of arms.',
+        'question' => 'Name a color on Zürich\'s flag',
         'answers' => 'white,blue,White,Blue'
       }
     ]
