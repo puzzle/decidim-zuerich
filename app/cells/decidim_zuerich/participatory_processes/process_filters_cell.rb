ffc05171c4479f3c46ac075614e2c22602851c13
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-03-29 12:50:52 +0200

    Fix process_filter_cell count override

--- app/cells/decidim_zuerich/participatory_processes/process_filters_cell.rb
+++ app/cells/decidim_zuerich/participatory_processes/process_filters_cell.rb
@@ -18,7 +18,7 @@ module DecidimZuerich
         return @process_count_by_filter if @process_count_by_filter
 
         @process_count_by_filter = %w(active upcoming past).inject({}) do |collection_by_filter, filter_name|
-          filtered_processes = filtered_processes(filter_name).results
+          filtered_processes = filtered_processes(filter_name)
           collection_by_filter.merge(filter_name => filtered_processes.count)
         end
         @process_count_by_filter["all"] = @process_count_by_filter.values.sum
