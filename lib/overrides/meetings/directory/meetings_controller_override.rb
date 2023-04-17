abc1bed18ba2a687b6b7802600bad018513fd033
Thomas Burkhalter <burkhalter@puzzle.ch>
2023-04-17 14:56:06 +0200

    Fix ransack query override

--- lib/overrides/meetings/directory/meetings_controller_override.rb
+++ lib/overrides/meetings/directory/meetings_controller_override.rb
@@ -1,5 +1,5 @@
 Decidim::Meetings::Directory::MeetingsController.class_eval do
   def meetings
-    @meetings ||= paginate(search.results.order(:start_time))
+    @meetings ||= paginate(search.result.order(:start_time))
   end
 end
