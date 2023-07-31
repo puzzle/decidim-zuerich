89c4f83b257d1699a501ebd3c1f90e8954019e6e
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-07-31 13:45:29 +0200

    Check includes for changes

--- app/events/decidim_zuerich/debates/create_debate_event.rb
+++ app/events/decidim_zuerich/debates/create_debate_event.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Override last checked at: 31.07.2023
+# TODO: @cbeltrame, check for 0.27.4 upgrade
+
 module DecidimZuerich
   module Debates
     module CreateDebateEvent
--- config/initializers/decidim_zuerich_customization.rb
+++ config/initializers/decidim_zuerich_customization.rb
@@ -4,7 +4,6 @@ require_relative '../../lib/customization_output'
 require_relative '../../lib/decidim_zuerich/form_builder'
 
 includes = [
-  [Decidim::Comments::CommentVotedEvent, DecidimZuerich::Comments::CommentVotedEvent],
   [Decidim::Debates::CreateDebateEvent,  DecidimZuerich::Debates::CreateDebateEvent],
   [Decidim::DiffCell,                    DecidimZuerich::DiffCell],
   [Decidim::FormBuilder,                 DecidimZuerich::FormBuilder]
