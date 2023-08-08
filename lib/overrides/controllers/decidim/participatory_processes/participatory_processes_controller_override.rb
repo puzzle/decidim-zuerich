193b7206ee8e9b34aae61f14b6a3f0dabba1bbe0
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-08-08 11:08:24 +0200

    Fix participatory_processes override

--- lib/overrides/controllers/decidim/participatory_processes/participatory_processes_controller_override.rb
+++ lib/overrides/controllers/decidim/participatory_processes/participatory_processes_controller_override.rb
@@ -2,20 +2,22 @@
 
 # Override last checked at: 31.07.2023
 Decidim::ParticipatoryProcesses::ParticipatoryProcessesController.class_eval do
-  helper_method :has_landing_page
+  helper_method :participatory_process_groups,
+                :filtered_processes,
+                :participatory_processes,
+                :has_landing_page
 
   private
 
   def collection
     # Don't include process groups in this list
-    @collection ||= paginate(Kaminari.paginate_array(participatory_processes))
+    @collection ||= participatory_processes
   end
 
   def has_landing_page(process_group)
     Decidim::ContentBlock
       .published
       .for_scope(:participatory_process_group_homepage, organization: current_organization)
-      .where(scoped_resource_id: process_group.id)
-      .exists?
+      .exists?(scoped_resource_id: process_group.id)
   end
 end
