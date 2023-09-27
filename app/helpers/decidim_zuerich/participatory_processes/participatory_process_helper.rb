e51036fd8f9263b45697680b4d6f6986144507d9
Carlo Beltrame <carlo@beltra.me>
2023-09-27 15:53:51 +0200

    Improve process step date display
    Refs #404

--- app/helpers/decidim_zuerich/participatory_processes/participatory_process_helper.rb
+++ app/helpers/decidim_zuerich/participatory_processes/participatory_process_helper.rb
@@ -6,7 +6,7 @@ module DecidimZuerich::ParticipatoryProcesses::ParticipatoryProcessHelper
     dates = [participatory_process_step.start_date, participatory_process_step.end_date].compact
 
     case dates.count
-    when 0 then "Kein Datum angegeben"
+    when 0 then I18n.t('decidim_zuerich.participatory_processes.participatory_process_helper.step_dates.no_dates')
     when 1 then localize(dates.first.to_date, format: :default)
     when 2 then "#{localize(dates.first.to_date, format: :default)} - #{localize(dates.second.to_date, format: :default)}"
     else        ""
