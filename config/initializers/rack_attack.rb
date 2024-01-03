33a941f59c0c798f4be4cfd442f847162c273bad
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-01-03 18:29:38 +0100

    Add general IP throttling in Rack::Attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -14,13 +14,15 @@ safelist_ips.each do |ip_or_subnet|
   Rack::Attack.safelist_ip(ip_or_subnet)
 end
 
+Rack::Attack.throttle('requests by ip', limit: 5, period: 2, &:ip)
+
 Rack::Attack.blocklist('secure admin logins') do |req|
   Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 5, findtime: 10.minutes, bantime: 1.hour) do
     req.post? && req.path.include?('system')
   end
 end
 
-ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, start, finish, request_id, payload|
+ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, _start, _finish, _request_id, payload|
   # request object available in payload[:request]
   request = payload[:request]
   # require 'pry'; binding.pry
@@ -31,7 +33,13 @@ end
 # # rubocop:disable Metrics/BlockLength
 # ActiveSupport::Notifications.subscribe('rack.attack') do |_name, _start, _finish, _request_id, req|
 #   req = req[:request]
-#   # msg = [req.env['rack.attack.match_type'], req.ip, req.request_method, req.fullpath, ('"' + req.user_agent.to_s + '"')].join(' ')
+#   # msg = [
+#   #   req.env['rack.attack.match_type'],
+#   #   req.ip,
+#   #   req.request_method,
+#   #   req.fullpath,
+#   #   ('"' + req.user_agent.to_s + '"')
+#   # ].join(' ')
 
 #   request_headers =
 #     req
