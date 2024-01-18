7b386c8add9cf1fcd5aa592a72147ed463b9dd99
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-01-18 13:11:26 +0100

    Force activation of Antivirus

--- config/initializers/participatory_documents.rb
+++ config/initializers/participatory_documents.rb
@@ -1,11 +1,15 @@
-# Decidim::ParticipatoryDocuments.configure do |config|
-#   # Public: The maximum length of any text field (body, answers, etc) to export.
-#   # Defaults to 50. Set to 0 to export the full text.
-#   config.max_export_text_length = 0
-#
-#   # Public: The minimum length of a suggestion to be considered valid.
-#   config.min_suggestion_length = 5
-#
-#   # Public: The maximum length of a suggestion to be considered valid.
-#   config.max_suggestion_length = 500
-# end
+# frozen_string_literal: true
+
+Decidim::ParticipatoryDocuments.configure do |config|
+  ## Public: The maximum length of any text field (body, answers, etc) to export.
+  ## Defaults to 50. Set to 0 to export the full text.
+  # config.max_export_text_length = 0
+
+  ## Public: The minimum length of a suggestion to be considered valid.
+  # config.min_suggestion_length = 5
+
+  ## Public: The maximum length of a suggestion to be considered valid.
+  # config.max_suggestion_length = 500
+
+  config.antivirus_enabled = true
+end
