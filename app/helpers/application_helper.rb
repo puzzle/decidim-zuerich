f1524844267fd6c9a1892cd23332880f593cb490
Thomas Burkhalter <tburkhalter@pm.me>
2024-09-30 21:27:03 +0200

    Fix formatting, add more bash functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
-module ApplicationHelper
+# frozen_string_literal: true
 
+module ApplicationHelper
   def number_to_currency(number, options = {})
     defaults = {
-        locale: :de,
-        unit: Decidim.currency_unit,
-        separator: ',',
-        delimiter: '\'',
-        format: '%u %n'
+      locale: :de,
+      unit: Decidim.currency_unit,
+      separator: ',',
+      delimiter: '\'',
+      format: '%u %n'
     }
     super number, defaults.merge(options)
   end
