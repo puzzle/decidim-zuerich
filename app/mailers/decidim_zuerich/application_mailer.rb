8c0694a2c4da5ce2a43a9a80795e47c4ffbf1e38
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-01-20 22:35:11 +0100

    Fix reply-to

--- app/mailers/decidim_zuerich/application_mailer.rb
+++ app/mailers/decidim_zuerich/application_mailer.rb
@@ -6,8 +6,13 @@ module DecidimZuerich
   # mailers.
   module ApplicationMailer
     def set_smtp
-      reply_to = I18n.t('decidim_zuerich.reply_to', default: "").presence
-      mail.reply_to = [reply_to] if mail.reply_to.blank? && reply_to
+      opts = { scope: 'decidim_zuerich.reply_to', default: '' }
+      tenant_type = organization.tenant_type.presence
+
+      reply_to = I18n.t(tenant_type, **opts).presence if tenant_type
+      reply_to ||= I18n.t(:default, **opts).presence
+
+      mail.reply_to = reply_to && [reply_to]
 
       super
     end
