a3b255bf9e12b44d83f39dc900e44114a23d0041
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-02-17 11:27:25 +0100

    Improve local setup

--- scripts/localize_database.rb
+++ scripts/localize_database.rb
@@ -5,7 +5,19 @@ exec('/usr/bin/env', 'rails', 'runner', $PROGRAM_NAME, *ARGV) unless defined?(Ra
 
 # Prepare the Database to work well locally
 class DevEnv
-  def self.localize_hosts
+
+  def self.run
+    new.run
+  end
+
+  def run
+    puts "\n\n"
+    localize_hosts
+    reset_password_timers
+    remove_mail_settings
+  end
+
+  def localize_hosts
     Decidim::Organization.all.each do |org|
       old_host = org.host
       parts = old_host.split('.')
@@ -17,14 +29,21 @@ class DevEnv
     end
   end
 
-  def self.reset_password_timers
+  def reset_password_timers
     # rubocop:disable Rails/SkipsModelValidations
     Decidim::User.update_all(password_updated_at: DateTime.now)
     # rubocop:enable Rails/SkipsModelValidations
 
     puts 'reseting all :password_updated_at fields'
   end
+
+  # This is needed so tools like mailcatcher work
+  def remove_mail_settings(smtp_settings=nil)
+    smtp_settings ||= {}
+    Decidim::Organization.update_all(smtp_settings: smtp_settings)
+
+    puts 'reseting all :smtp_settings fields'
+  end
 end
 
-DevEnv.localize_hosts
-DevEnv.reset_password_timers
+DevEnv.run
