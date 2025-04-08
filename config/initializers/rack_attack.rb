bf8a8b516a1716343d74b5a087aeb1399576ad23
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-04-08 19:43:02 +0200

    Improve Rack::Attack configuration

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,84 +1,18 @@
+# frozen_string_literal: true
+
 require 'rack/attack'
 require 'ipaddr'
+require_relative '../../lib/rack_attack_helper'
 
-Rack::Attack.enabled = ENV.fetch('ENABLE_RACK_ATTACK', Rails.env.production?.to_s).in?(%w[true 1])
-# Rack::Attack.enabled = true
+rack = DecidimZuerich::RackAttackHelper
 
-safelist_ips = ENV.fetch('RACK_ATTACK_SAFELIST_IPS', '').split(',').map(&:strip)
-
-safelist_ips.each do |ip_or_subnet|
-  # Fails if the ip/subnet is not correct
-  IPAddr.new(ip_or_subnet)
-
-  Rack::Attack.safelist_ip(ip_or_subnet)
+Rack::Attack.enabled = rack.enabled?
+rack.safelist_ips_from_env
+rack.register_s3_redirects
+rack.register_throttle_filter_from_env 'requests by ip', 'RACK_ATTACK_FILTER_BY_IP'
+rack.register_allow2ban_filter_from_env 'secure admin logins', 'RACK_ATTACK_FILTER_ADMIN_LOGIN' do |req|
+  req.post? && req.path.include?('system')
 end
 
-Rack::Attack.safelist 'allow S3 redirects' do |request|
-  regexes = [
-    %r{\Ahttps://[^/]+?/rails/active_storage/blobs/redirect/[A-Za-z0-9=]+--[A-Za-z0-9=]+/},
-    %r{\Ahttps://[^/]+?/rails/active_storage/representations/redirect/[A-Za-z0-9=]+--[A-Za-z0-9=]+/[A-Za-z0-9=]+--[A-Za-z0-9=]+/}
-  ]
-
-  regexes.any? { _1.match? request.url }
-end
-
-Rack::Attack.throttle('requests by ip', limit: 100, period: 10, &:ip)
-
-Rack::Attack.blocklist('secure admin logins') do |req|
-  Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 5, findtime: 10.minutes, bantime: 1.hour) do
-    req.post? && req.path.include?('system')
-  end
-end
-
-ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, _start, _finish, _request_id, payload|
-  # request object available in payload[:request]
-  request = payload[:request]
-  # require 'pry'; binding.pry
-  Rails.logger.warn "RACK ATTACK MATCH: #{name}: #{request.env['rack.attack.matched']} | ip: #{request.ip} | url: #{request.url}"
-end
-
-# Used for rack-attack throttling debugging
-#
-# ActiveSupport::Notifications.subscribe('rack.attack') do |_name, _start, _finish, _request_id, req|
-#   req = req[:request]
-#   # msg = [
-#   #   req.env['rack.attack.match_type'],
-#   #   req.ip,
-#   #   req.request_method,
-#   #   req.fullpath,
-#   #   ('"' + req.user_agent.to_s + '"')
-#   # ].join(' ')
-
-#   request_headers =
-#     req
-#     .headers
-#     .env
-#     .except(*%w[warden HTTP_COOKIE]
-#     .reject { |key| key.to_s.include?('.') }
-
-#   request_params = req.params.to_enum.to_h
-
-#   hash = {
-#     match_type: req.env['rack.attack.match_type'],
-#     request: {
-#       request_method: request.method,
-#       ip: request.ip,
-#       remote_ip: request.remote_ip,
-#       url: request.url,
-#       fullpath: request.fullpath,
-#       user_agent: request.user_agent.to_s,
-#       headers: request_headers,
-#       params: request_params
-#     }
-#   }
-
-#   msg = Hash[*hash.sort.flatten].to_json
-
-#   logger = Logger.new('log/rack-attack.log')
-
-#   if %i[throttle blocklist].include?(req.env['rack.attack.match_type'])
-#     logger.error(msg)
-#   else
-#     logger.info(msg)
-#   end
-#   # end
+rack.subscribe_to_notifications
+rack.subscribe_to_debug_notifications if rack.debug?
