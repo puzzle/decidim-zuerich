b9afd32088342a2ecfa1212b2e6c6e6af37fec27
carlobeltrame <carlo@beltra.me>
2025-05-26 17:12:09 +0200

    Remove scrolling customization
    Refs #510

--- app/packs/src/decidim/decidim_application.js
+++ app/packs/src/decidim/decidim_application.js
@@ -44,19 +44,6 @@ window.addEventListener('scroll', () => {
   }
 })
 
-window.addEventListener("load", (event) => {
-  const process_nav = document.querySelector('#process-nav-content > ul');
-  if (process_nav == null) {
-    return;
-  }
-
-  const active_child = process_nav.querySelector('li + li.is-active');
-
-  if (active_child != null) {
-    process_nav.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
-  }
-});
-
 /**
  * Redirect to external participatory processes
  */
