867dfc7885fd82da7f66f8fe435145b17380d69e
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-10-23 14:38:43 +0200

    Update map provider backend code

--- app/provider/decidim/map/provider/dynamic_map/gis_zh.rb
+++ app/provider/decidim/map/provider/dynamic_map/gis_zh.rb
@@ -5,18 +5,20 @@ module Decidim
         class GisZh < ::Decidim::Map::DynamicMap
 
           def builder_options
-            configuration.merge(super)
+            configuration
+              .merge(super)
+              .symbolize_keys
+              .deep_merge(organization.map_config.symbolize_keys)
           end
 
           # A builder for the GIS LU maps which need to be configured differently
           # than "normal" OSM based tile service providers.
           class Builder < Decidim::Map::DynamicMap::Builder
-            # @see Decidim::Map::DynamicMap::Builder#javascript_snippets
-            def javascript_snippets
-              [
-                  template.javascript_pack_tag("decidim/gis_zh"),
-                  template.javascript_pack_tag("decidim_proposals")
-              ].join
+            # @see Decidim::Map::DynamicMap::Builder#append_assets
+            def append_assets
+              template.append_stylesheet_pack_tag("decidim_map")
+              template.append_javascript_pack_tag("decidim/gis_zh", defer: false)
+              template.append_javascript_pack_tag("decidim_proposals", defer: false)
             end
           end
         end
--- app/provider/decidim/map/provider/dynamic_map/swisstopo.rb
+++ app/provider/decidim/map/provider/dynamic_map/swisstopo.rb
@@ -3,20 +3,21 @@ module Decidim
     module Provider
       module DynamicMap
         class Swisstopo < ::Decidim::Map::DynamicMap
-
           def builder_options
-            configuration.merge(super)
+            configuration
+              .merge(super)
+              .symbolize_keys
+              .deep_merge(organization.map_config.symbolize_keys)
           end
 
           # A builder for the GIS LU maps which need to be configured differently
           # than "normal" OSM based tile service providers.
           class Builder < Decidim::Map::DynamicMap::Builder
-            # @see Decidim::Map::DynamicMap::Builder#javascript_snippets
-            def javascript_snippets
-              [
-                  template.javascript_pack_tag("decidim/swisstopo"),
-                  template.javascript_pack_tag("decidim_proposals")
-              ].join
+            # @see Decidim::Map::DynamicMap::Builder#append_assets
+            def append_assets
+              template.append_stylesheet_pack_tag('decidim_map')
+              template.append_javascript_pack_tag('decidim/swisstopo', defer: false)
+              template.append_javascript_pack_tag('decidim_proposals', defer: false)
             end
           end
         end
