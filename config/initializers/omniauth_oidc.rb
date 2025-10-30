620e5c277b16442a0c9e5427b89c3243d67cd165
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-10-30 17:22:34 +0100

    Register proposals icon

--- config/initializers/omniauth_oidc.rb
+++ config/initializers/omniauth_oidc.rb
@@ -1,23 +1,24 @@
 if Rails.application.secrets.dig(:omniauth, :oidc).present?
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider(
-        :oidc,
-        setup: ->(env) {
-          request = Rack::Request.new(env)
-          organization = Decidim::Organization.find_by(host: request.host)
-          provider_config = organization.enabled_omniauth_providers[:oidc]
-          env["omniauth.strategy"].options[:client_options] ||= {}
-          env["omniauth.strategy"].options[:issuer] = provider_config[:issuer]
-          env["omniauth.strategy"].options[:client_options][:identifier] = provider_config[:client_id]
-          env["omniauth.strategy"].options[:client_options][:secret] = provider_config[:client_secret]
-          env["omniauth.strategy"].options[:client_options][:redirect_uri] = provider_config[:redirect_url]
-        },
-        name: :oidc,
-        discovery: true,
-        client_auth_method: :basic,
-        scope: [:openid, :stzh_profile_basic],
+      :oidc,
+      setup: lambda { |env|
+        request = Rack::Request.new(env)
+        organization = Decidim::Organization.find_by(host: request.host)
+        provider_config = organization.enabled_omniauth_providers[:oidc]
+        env['omniauth.strategy'].options[:client_options] ||= {}
+        env['omniauth.strategy'].options[:issuer] = provider_config[:issuer]
+        env['omniauth.strategy'].options[:client_options][:identifier] = provider_config[:client_id]
+        env['omniauth.strategy'].options[:client_options][:secret] = provider_config[:client_secret]
+        env['omniauth.strategy'].options[:client_options][:redirect_uri] = provider_config[:redirect_url]
+      },
+      name: :oidc,
+      discovery: true,
+      client_auth_method: :basic,
+      scope: %i[openid stzh_profile_basic]
     )
   end
 
   Decidim.icons.register(name: 'oidc-fill', icon: 'oidc', category: 'system', description: '', engine: :core)
+  Decidim.icons.register(name: 'proposals', icon: 'proposals', category: 'system', description: '', engine: :core)
 end
